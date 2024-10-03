d2ff2a75ad9dc538d44172df2b11fc9b28c76f7e
rodrigogala <128104438+rodrigogala@users.noreply.github.com>
2024-10-03 08:50:19 -0300

    script.js

--- script.js
+++ script.js
@@ -10,11 +10,9 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Atividades agricolas!",
-                afirmacao: "Atividades industriais "
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                texto: "Atividades industriais",
             }
         ]
     },
@@ -23,11 +21,9 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Biodiversidade",
-                afirmacao: "Clima."
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "Clima.",
             }
         ]
     },
@@ -36,11 +32,9 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Politicas governamentais",
-                afirmacao: "Conscientização da população."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "Conscientização da população.",
             }
         ]
     },
@@ -49,11 +43,9 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Preservação de florestas existentes"
-                afirmacao:Reflorestamento de areas degradadas
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "Reflorestamento de areas degradadas.",
             }
         ]
     },
