e11f9e8166bc8ce7e3c4af13fca7329a99406dff
rodrigogala <128104438+rodrigogala@users.noreply.github.com>
2024-10-03 09:07:30 -0300

    script.js

--- script.js
+++ script.js
@@ -6,34 +6,62 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Vaçê acha que o desflorestamento e mais causado por atividades agricolas ou industriais?",
+        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
         alternativas: [
-            "Atividades Agricolas",
-            "Atividades Industriais"
+            {
+                texto: "Isso é assustador!",
+            },
+            {
+                texto: "Isso é maravilhoso!",
+            }
         ]
     },
     {
-        enunciado: "Na sua opinião o desflorestamento tem mais impacto na biodiversidade ou no clina?",
+        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
         alternativas: [
-            "Biodiversidade",
-            "Clima"
+            {
+                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+            },
+            {
+                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+            }
         ]
     },
     {
-        enunciado: "você acredita que a solução para o desflorestamento está mais ligada a politicas governamentais ou a conscientização da população?",
+        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
         alternativas: [
-            "Politicas governamentais",
-            "Conscientização da população"
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+            }
         ]
     },
     {
-        enunciado: "Na Sua perspectiva o combate ao desflorestamento deveria priorizar a preservação de florestas degradadas?",
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
         alternativas: [
-            "Preservação de florestas existentes",
-            "Reflorestamento de areas degradadas"
+            {
+                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+            {
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
         ]
     },
-    let atual = 0;
+    {
+        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        alternativas: [
+            {
+                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+            },
+            {
+                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+            }
+        ]
+    },
+];
+
+
+let atual = 0;
 let perguntaAtual;
 let historiaFinal = "";
 
