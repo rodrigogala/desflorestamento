aa8c8d78650482464416b8256165020c1e6ad461
rodrigogala <128104438+rodrigogala@users.noreply.github.com>
2024-10-03 09:00:58 -0300

    script.js

--- script.js
+++ script.js
@@ -6,100 +6,38 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "1-Você acha que o desflorestamento e mais causado por atividades agricolas ou industriais?"
+        enunciado: "Vaçê acha que o desflorestamento e mais causado por atividades agricolas ou industriais?",
         alternativas: [
-            {
-                texto: "Atividades agricolas!",
-            },
-            {
-                texto: "Atividades industriais",
-            }
+            "Atividades Agricolas",
+            "Atividades Industriais"
         ]
     },
     {
-        enunciado: "2-Na sua opinião o desflorestamento tem mais impacto na biodiversidade ou no clima?",
+        enunciado: "Na sua opinião o desflorestamento tem mais impacto na biodiversidade ou no clina?",
         alternativas: [
-            {
-                texto: "Biodiversidade",
-            },
-            {
-                texto: "Clima.",
-            }
+            "Biodiversidade",
+            "Clima"
         ]
     },
     {
-        enunciado: "3-Vocẽ acredita que a solução para o desflorestamento está mais ligada a politicas governamentais ou a conscientizaçãp da popilação?",
+        enunciado: "você acredita que a solução para o desflorestamento está mais ligada a politicas governamentais ou a conscientização da população?",
         alternativas: [
-            {
-                texto: "Politicas governamentais",
-            },
-            {
-                texto: "Conscientização da população.",
-            }
+            "Politicas governamentais",
+            "Conscientização da população"
         ]
     },
     {
-        enunciado: "4-Na sua perspectiva o combate ao desflorestamento deveria priorizar a preservação de florestas existentes ou reflorestamento de areas degradadas?",
+        enunciado: "Na Sua perspectiva o combate ao desflorestamento deveria priorizar a preservação de florestas degradadas?",
         alternativas: [
-            {
-                texto: "Preservação de florestas existentes"
-            },
-            {
-                texto: "Reflorestamento de areas degradadas.",
-            }
+            "Preservação de florestas existentes",
+            "Reflorestamento de areas degradadas"
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "Pergunta 5",
         alternativas: [
-            {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
-            },
-            {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
-            }
+            "Alternativa 1",
+            "Alternativa 2"
         ]
-    },
+    }
 ];
-
-
-let atual = 0;
-let perguntaAtual;
-let historiaFinal = "";
-
-function mostraPergunta() {
-    if (atual >= perguntas.length) {
-        mostraResultado();
-        return;
-    }
-    perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent = perguntaAtual.enunciado;
-    caixaAlternativas.textContent = "";
-    mostraAlternativas();
-}
-
-function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas) {
-        const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
-        caixaAlternativas.appendChild(botaoAlternativas);
-    }
-}
-
-function respostaSelecionada(opcaoSelecionada) {
-    const afirmacoes = opcaoSelecionada.afirmacao;
-    historiaFinal += afirmacoes + " ";
-    atual++;
-    mostraPergunta();
-}
-
-function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
-    textoResultado.textContent = historiaFinal;
-    caixaAlternativas.textContent = "";
-}
-
-mostraPergunta();
